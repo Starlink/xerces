53c038cbefb0840c14a6bf6ce71b8d20f7ca63c8
Graham Bell <g.bell@eaobservatory.org>
2025-04-08 17:04:02 -1000

    Import v3.3.0

--- src/xercesc/util/NetAccessors/Curl/CurlURLInputStream.cpp
+++ src/xercesc/util/NetAccessors/Curl/CurlURLInputStream.cpp
@@ -39,6 +39,8 @@
   #include <sys/time.h>
 #endif
 
+#include <algorithm>
+
 #include <xercesc/util/XercesDefs.hpp>
 #include <xercesc/util/XMLNetAccessor.hpp>
 #include <xercesc/util/NetAccessors/Curl/CurlURLInputStream.hpp>
@@ -52,6 +54,7 @@
 
 XERCES_CPP_NAMESPACE_BEGIN
 
+#define MAX_CURL_ALLOC_SIZE 1073741824
 
 CurlURLInputStream::CurlURLInputStream(const XMLURL& urlSource, const XMLNetHTTPInfo* httpInfo/*=0*/)
       : fMulti(0)
@@ -64,12 +67,18 @@ CurlURLInputStream::CurlURLInputStream(const XMLURL& urlSource, const XMLNetHTTP
       , fBytesRead(0)
       , fBytesToRead(0)
       , fDataAvailable(false)
-      , fBufferHeadPtr(fBuffer)
-      , fBufferTailPtr(fBuffer)
+      , fBuffer(0)
+      , fBufferHeadPtr(0)
+      , fBufferTailPtr(0)
+      , fBufferSize(0)
       , fPayload(0)
       , fPayloadLen(0)
       , fContentType(0)
 {
+    if (urlSource.isRelative()) {
+        ThrowXML(MalformedURLException, XMLExcepts::URL_NoProtocolPresent);
+    }
+
     // Allocate the curl multi handle
     fMulti = curl_multi_init();
 
@@ -212,6 +221,12 @@ void CurlURLInputStream::cleanup()
 
     if(fHeadersList) curl_slist_free_all(fHeadersList);
     fHeadersList = NULL;
+
+    if(fBuffer) fMemoryManager->deallocate(fBuffer);
+    fBuffer = NULL;
+    fBufferHeadPtr = NULL;
+    fBufferTailPtr = NULL;
+    fBufferSize = 0;
 }
 
 
@@ -257,13 +272,33 @@ CurlURLInputStream::writeCallback(char *buffer,
 	cnt				-= consume;
 	if (cnt > 0)
 	{
-		XMLSize_t bufAvail = sizeof(fBuffer) - (fBufferHeadPtr - fBuffer);
-		consume = (cnt > bufAvail) ? bufAvail : cnt;
-		memcpy(fBufferHeadPtr, buffer, consume);
-		fBufferHeadPtr	+= consume;
-		buffer			+= consume;
-		totalConsumed	+= consume;
-		//printf("write callback rebuffering %d bytes\n", consume);
+                XMLSize_t bufAvail = fBufferSize - (fBufferHeadPtr - fBuffer);
+                if (bufAvail < cnt) {
+                    // Enlarge the buffer.
+                    XMLSize_t newsize = fBufferSize + (cnt - bufAvail);
+                    if (newsize > MAX_CURL_ALLOC_SIZE) {
+                        return 0;
+                    }
+                    XMLByte* newbuf = reinterpret_cast<XMLByte*>(fMemoryManager->allocate(newsize));
+                    if (!newbuf) {
+                        // Enlarge attempt failed, signal error back to libcurl.
+                        // The dedicated error code is a recent libcurl addition so is not portable.
+                        return 0;
+                    }
+                    // Not a realloc, so we have to copy the data from old to new.
+                    memcpy(newbuf, fBuffer, fBufferHeadPtr - fBuffer);
+                    fBufferSize = newsize;
+                    //printf("enlarged buffer to %u bytes", fBufferSize);
+                    fBufferHeadPtr = newbuf + (fBufferHeadPtr - fBuffer);
+                    fMemoryManager->deallocate(fBuffer);
+                    fBuffer = fBufferTailPtr = newbuf;
+                }
+
+                memcpy(fBufferHeadPtr, buffer, cnt);
+                fBufferHeadPtr  += cnt;
+                buffer += cnt;
+                totalConsumed   += cnt;
+                //printf("write callback rebuffering %u bytes", cnt);
 	}
 
 	// Return the total amount we've consumed. If we don't consume all the bytes
@@ -332,7 +367,21 @@ bool CurlURLInputStream::readMore(int *runningHandles)
             break;
 
         default:
-            ThrowXMLwithMemMgr1(NetAccessorException, XMLExcepts::NetAcc_InternalError, fURLSource.getURLText(), fMemoryManager);
+            {
+              struct CurlError
+              {
+                  XMLCh* fErrorString;
+                  MemoryManager* fMemoryManager;
+                  CurlError(CURLMsg* msg, MemoryManager* fMemoryManager) : fErrorString(XMLString::transcode(curl_easy_strerror(msg->data.result))), fMemoryManager(fMemoryManager) {}
+                  ~CurlError() { XMLString::release(&fErrorString, fMemoryManager); }
+              };
+              CurlError curlErrorStr(msg, fMemoryManager);
+
+              XMLCh curlErrorNumberStr[128];
+              XMLString::binToText(msg->data.result, curlErrorNumberStr, 127, 10, fMemoryManager);
+
+              ThrowXMLwithMemMgr3(NetAccessorException, XMLExcepts::NetAcc_CurlError, fURLSource.getURLText(), curlErrorNumberStr, curlErrorStr.fErrorString, fMemoryManager);
+            }
             break;
         }
     }
